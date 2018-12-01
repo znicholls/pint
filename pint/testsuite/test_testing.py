6f94a143e0ab900c3d5639343d245209a23a4a67
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-12-01 22:49:08 +1100

    Improve error message test

--- pint/testsuite/test_testing.py
+++ pint/testsuite/test_testing.py
@@ -21,9 +21,22 @@ class TestTesting(QuantityTestCase):
 
     def test_unequal_error(self):
         a = np.array([1, 2]) * UREG("m")
-        b = np.array([1, 2]) * UREG("m")
+        b = np.array([1, 2]) * UREG("km")
 
         with self.assertRaises(AssertionError) as context:
             assert_array_equal(a, b)
 
+        expected_error_msg = (
+            "numpy bits\n"
+            "\n"
+            "Note: values above have been converted to m\n"
+            "\n"
+            "Input units:\n"
+            "x: {}\n"
+            "y: {}\n"
+            "\n"
+            "Numerical values with units:\n"
+            "x: {}\n"
+            "y: {}".format(a.units, b.units, a, b)
+        )
         self.assertTrue("This is broken" in context.exception)
